bb9e65043f694f2d36225150a373007ec8dffc3d
peiqi <peiqi@qq.com>
2019-06-14 18:35:05 +0800

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Button from 'antd/lib/button'
 // import 'antd/dist/antd.css'
+//推送远程dev分支1111
 import {HashRouter,Route,Switch} from 'react-router-dom'
 import Longin from './pages/login/Login'
 import Admin from './pages/admin/Admin'
