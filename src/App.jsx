706ad2d14ff2cabf272b48d3c1b04c89f7980cf4
peiqi <peiqi@qq.com>
2019-06-14 19:43:42 +0800

    1.01

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import React from 'react'
-// import Button from 'antd/lib/button'
-// import 'antd/dist/antd.css'
-//推送远程dev分支111122222222333333344444
-import {HashRouter,Route,Switch} from 'react-router-dom'
-import Longin from './pages/login/Login'
-import Admin from './pages/admin/Admin'
+import React,{Component} from 'react'
+import { Button } from 'antd'
 
-
-export default class App extends React.Component{
+export default class App extends Component{
     render(){
         return(
-           <HashRouter>
-               <Switch>
-                   <Route path='/' component={Longin} />
-                   <Route path='/admin' component={Admin} />
-               </Switch>
-           </HashRouter>
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
         )
     }
 }
